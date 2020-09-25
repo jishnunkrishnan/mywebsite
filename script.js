82330efc58ed5fcdd2685e34453fa8d647c1084c
Jishnu N Krishnan <36995791+jishnunkrishnan@users.noreply.github.com>
2020-09-25 14:56:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function buttonClick() {
+    var name = document.forms["myForm"]["name"].value
+    var email = document.forms["myForm"]["emailid"].value
+    var contact = document.forms["myForm"]["contactno"].value
+
+    if(name == "" && email == "" && contact == "") {
+
+        alert("Name, Email, & Contact no. must be filled")
+        return false
+    } else {
+
+        var text = "You'll be contacted soon!"
+        document.getElementById("display-info").innerHTML=text
+        return false
+    }
+
+}
